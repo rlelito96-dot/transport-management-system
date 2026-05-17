a15b72500c8f66df0d0448d8dc83a7bc4ecf7330
rafal <rlelito96@gmail.com>
2026-05-18 00:07:42 +0200

    feat(auth): add login endpoint

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import Session
 from app.api.dependencies.db import get_db
 from app.application.dto.user_dto import (
     UserCreateDTO,
+    UserLoginDTO,
     UserResponseDTO,
 )
-from app.application.use_cases.auth import RegisterUserUseCase
+from app.application.use_cases.auth import LoginUserUseCase, RegisterUserUseCase
 from app.infrastructure.repositories.user_repo import UserRepository
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -31,5 +32,28 @@ def register_user(
     except ValueError as error:
         raise HTTPException(
             status_code=400,
+            detail=str(error),
+        ) from error
+
+
+@router.post("/login")
+def login_user(
+    user_data: UserLoginDTO,
+    db: Session = Depends(get_db),
+):
+    repo = UserRepository(db)
+    use_case = LoginUserUseCase(repo)
+
+    try:
+        user = use_case.execute(
+            email=user_data.email,
+            password=user_data.password,
+        )
+
+        return user
+
+    except ValueError as error:
+        raise HTTPException(
+            status_code=401,
             detail=str(error),
         ) from error
