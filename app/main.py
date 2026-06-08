b3b27b819bfd0a267ab2ab9bfe98627f7f65cc43
rafal <rlelito96@gmail.com>
2026-06-08 22:15:45 +0200

    chore(app): add tracking router to main app

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from app.api.routers.auth import router as auth_router
 from app.api.routers.orders import router as order_router
+from app.api.routers.tracking import router as tracking_router
 
 app = FastAPI(title="TMS Backend")
 
 app.include_router(auth_router)
 app.include_router(order_router)
+app.include_router(tracking_router)
 
 
 @app.get("/")
