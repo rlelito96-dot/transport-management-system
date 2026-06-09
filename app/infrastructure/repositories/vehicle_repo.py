26e713e35a06a422a2f480b3dadcf387d9f92cdc
rafal <rlelito96@gmail.com>
2026-06-09 23:53:17 +0200

    feat(vehicle): add create method in repository

--- app/infrastructure/repositories/vehicle_repo.py
+++ app/infrastructure/repositories/vehicle_repo.py
@@ -9,3 +9,16 @@ class VehicleRepository:
 
     def get_by_id(self, vehicle_id: int):
         return self.db.query(Vehicle).filter(Vehicle.id == vehicle_id).first()
+
+    def create(self, plate_number: str, capacity_kg: int, company_id: int):
+        vehicle = Vehicle(
+            plate_number=plate_number,
+            capacity_kg=capacity_kg,
+            company_id=company_id,
+        )
+
+        self.db.add(vehicle)
+        self.db.commit()
+        self.db.refresh(vehicle)
+
+        return vehicle
